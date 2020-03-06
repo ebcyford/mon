61235236c64bf12fafb62d72fd4a62265694917f
ebcyford <ebcyford@gmail.com>
2020-03-05 21:48:10 -0500

    changed name

--- mon/TIF_to_HDF5.py
+++ mon/TIF_to_HDF5.py
@@ -16,6 +16,7 @@ from tqdm import tqdm
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 DEFAULT_OUT = os.path.join(DATA_DIR, "tif_to_hdf5.h5")
+DEFAULT_DATA_COLS = ["1_R", "2_G", "3_B"]
 
 parser = argparse.ArgumentParser(
     description="Read TIF chips into singular HDF5 file")
@@ -26,28 +27,34 @@ parser.add_argument("--out_file", type=str, default=DEFAULT_OUT,
                     help="output file of chips [default:/data/tif_to_hdf5.h5")
 parser.add_argument("--classes", nargs="+", default=["not_spruce", "spruce"],
                     help="list of classifications to look for [default:not_spruce, spruce")
+parser.add_argument("--data_cols", nargs="+", default=DEFAULT_DATA_COLS,
+                    help="ordered list of channels in TIF file [default:1_R, 2_G, 3_B]")
 
 FLAGS = parser.parse_args()
 
 IN_DIR = FLAGS.in_dir
 OUT_FILE = FLAGS.out_file
 CLASSIFICATIONS = FLAGS.classes
-DT = np.dtype("uint8")
-DATA_COLS = ["1_R", "2_G", "3_B"]
+DATA_COLS = FLAGS.data_cols
+DT = np.dtype(float)
 
 if not os.path.exists(DATA_DIR): os.mkdir(DATA_DIR)
 
 counter = 0
 with h5py.File(OUT_FILE, "w") as f:
     for classification in CLASSIFICATIONS:
-        for img in tqdm(os.listdir(os.path.join(IN_DIR, classification))):
+        for img in tqdm(os.listdir(os.path.join(IN_DIR, classification)), 
+                        desc="Writing Class: {}".format(classification)):
+
+            # Group names are simply increasing numbers
             img_name = counter
             img_path = os.path.join(IN_DIR, classification, img)
             grp = f.create_group("{}".format(img_name))
 
-            open_img = Image.open(img_path)
-            img_arr = np.array(open_img)
+            # Read TIF file as a numpy array
+            img_arr = np.array(Image.open(img_path))
 
+            # Write each channel as a separate dataset
             for i in DATA_COLS: 
                 idx = DATA_COLS.index(i)
                 grp.create_dataset("{}".format(i), data=img_arr[:, :, idx:idx+1], dtype=DT)
