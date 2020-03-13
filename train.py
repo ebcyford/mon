318ff56206478487960f3e93f46d451fe420b931
ebcyford <ebcyford@gmail.com>
2020-03-13 13:53:18 -0400

    updated model and argument parser, added k-fold cross validation

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import numpy as np
 import tensorflow as tf
 from datetime import datetime
 from mon.utils import prep_chip
+from sklearn.model_selection import KFold
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, \
@@ -71,7 +72,7 @@ def get_data(training_file, img_size=80):
     return X, y
 
 
-def build_model(in_shape):
+def build_model(in_shape, learning_rate=0.0001):
     """Returns a tensorflow.python.keras.saving.saved_model
 
     Arguments:
@@ -101,6 +102,15 @@ def build_model(in_shape):
     
     model.add(Dense(1))
     model.add(Activation("sigmoid"))
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, 
+                                         beta_1=0.9,
+                                         beta_2=0.999,
+                                         epsilon=1e-07,
+                                         amsgrad=False)
+    model.compile(loss="binary_crossentropy",
+                  optimizer=optimizer, 
+                  metrics=["accuracy", tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
     
     return model
 
@@ -110,84 +120,86 @@ def main():
     print("Retrieving Data...")
     features, labels = get_data(IN_FILE, img_size=IMG_SIZE)
 
-    # Build and compile model
-    print("Building Model...")
-    model = build_model(features.shape[1:])
-    adam = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE, 
-                                    beta_1=0.9,
-                                    beta_2=0.999,
-                                    epsilon=1e-07,
-                                    amsgrad=False)
-    model.compile(loss="binary_crossentropy",
-                  optimizer=adam, 
-                  metrics=["accuracy"])
-    model.summary()
+    # Cross validation
+    cross_val = KFold(n_splits=FOLDS)
+    fold = 1
 
-    # Train model on training data
-    print("Training Model...")
-    model.fit(features, labels, 
-            batch_size=BATCH_SIZE, 
-            epochs=EPOCHS, 
-            validation_split=VALIDATION_SIZE, 
-            callbacks=[tensorboard])
+    # Train and run model
+    for train_idx, test_idx in cross_val.split(features):
+            X_train, X_test = features[train_idx], features[test_idx]
+            y_train, y_test = labels[train_idx], labels[test_idx]
+            
+            log_path = os.path.join(LOG_DIR, NAME + "_fold" + str(fold))
 
-    print("Saving...")
-    model.save(SAVE_FILE)
-    print("Model Saved to {}".format(SAVE_FILE))
+            save_file = os.path.join(SAVE_DIR, "{}_fold{}.model".format(NAME, fold))
+
+            fold += 1
+
+            print("Model File: " + save_file)
+            print("Log Path: " + log_path)
+            
+            # tensorboard = TensorBoard(log_dir=log_path, histogram_freq=1)
+            
+            # model = build_model(in_shape=X_train.shape[1:])
+            # model.fit(x=X_train, y=y_train,
+            #         batch_size=BATCH_SIZE,
+            #         epochs=EPOCHS,
+            #         validation_data=(X_test, y_test),
+            #         callbacks=[tensorboard])
+            
+            # model.save(save_file)
 
 
 if __name__ == "__main__":
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_DIR = os.path.join(BASE_DIR, "data")
-    DEFAULT_IN = os.path.join(BASE_DIR, "training_data.h5")
-    DEFAULT_LOG_DIR = os.path.join(BASE_DIR, "logs")
-    TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
-    DEFAULT_LOG = os.path.join(DEFAULT_LOG_DIR, TIMESTAMP)
     MODEL_DIR = os.path.join(BASE_DIR, "models")
-    DEFAULT_SAVE = os.path.join(MODEL_DIR, "model_" + TIMESTAMP)
+    LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+    TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
+    DEFAULT_IN = os.path.join(DATA_DIR, "training_data.h5")
+    DEFAULT_NAME = TIMESTAMP
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--in_file", type=str, default=DEFAULT_IN,
                         help="HDF5 file of training data chips [default:data\\training_data.h5")
-    parser.add_argument("--log_dir", type=str, default=DEFAULT_LOG_DIR,
-                        help="directory of log files [default:logs")
-    parser.add_argument("--save_file", type=str, default=DEFAULT_SAVE, 
-                        help="where to save model file [default:models\\model_+TIMESTAMP.model]")
-    parser.add_argument("--batch_size", type=int, default=512,
-                        help="number images to process in each batch [default:512]")
-    parser.add_argument("--epochs", type=int, default=20, 
-                        help="number of epochs to train model [default:20]")
-    parser.add_argument("--validation_size", type=int, default=0.1,
-                        help="size of validations set [default:0.1]")
-    parser.add_argument("--learning_rate", type=float, default=0.0001,
-                        help="learning rate for ADAM optimizer")
-    parser.add_argument("--name", type=str, default=DEFAULT_LOG,
+    parser.add_argument("--log_dir", type=str, default=LOG_DIR,
+                        help="directory to save log files [default:logs]")
+    parser.add_argument("--model_dir", type=str, default=MODEL_DIR,
+                        help="directory to save models [default:models]")
+    parser.add_argument("--name", type=str, default=DEFAULT_NAME,
                         help="name of model, timestamp appended")
-    parser.add_argument("--img_size", type=int, default=80, 
+    parser.add_argument("--batch_size", type=int, default=512, 
+                        help="number of images to process in each batch [default:512]")
+    parser.add_argument("--epochs", type=int, default=14,
+                        help="number of epochs to train model [default:14]")
+    parser.add_argument("--folds", type=int, default=10,
+                        help="number of fold in k-fold cross validation [default:10]")
+    parser.add_argument("--learning_rate", type=float, default=0.0001,
+                        help="learning rate for ADAM optimizer [default:0.0001]")
+    parser.add_argument("--img_size", type=int, default=80,
                         help="height of square image chip in pixels [default:80]")
 
     FLAGS = parser.parse_args()
 
-    IN_FILE = FLAGS.in_file
+    IN_FILE = FLAGS.in_file 
     LOG_DIR = FLAGS.log_dir
-    SAVE_FILE = FLAGS.save_file
+    MODEL_DIR = FLAGS.model_dir
+    NAME = FLAGS.name
     BATCH_SIZE = FLAGS.batch_size
     EPOCHS = FLAGS.epochs
-    VALIDATION_SIZE = FLAGS.validation_size
+    FOLDS = FLAGS.folds
     LEARNING_RATE = FLAGS.learning_rate
-    NAME = FLAGS.name
     IMG_SIZE = FLAGS.img_size
 
-    if NAME != DEFAULT_LOG:
-        log_path = os.path.join(LOG_DIR, NAME + "_" + TIMESTAMP)
-    else: 
-        log_path = os.path.join(LOG_DIR, NAME)
+    if NAME != DEFAULT_NAME:
+        NAME = NAME + "_" + TIMESTAMP
 
-    print("Logging to: " + log_path)
-    tensorboard = TensorBoard(log_dir=log_path, histogram_freq=1)
+    SAVE_DIR = os.path.join(MODEL_DIR, NAME)
 
     if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
     if not os.path.exists(MODEL_DIR): os.mkdir(MODEL_DIR)
+    if not os.path.exists(SAVE_DIR): os.mkdir(SAVE_DIR)
 
     main()
